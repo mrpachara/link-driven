eab8aab1d6f52cecf325a1a87366f64e830df864
mrpachara <mrpachara@gmail.com>
2015-12-08 19:05:36 +0700

    update to support windows

--- test/BASE.php
+++ test/BASE.php
@@ -1,15 +1,17 @@
 <?php
-	function contextPath(){
+	function contextPath($relative = null){
 		$dir = __DIR__;
 		$doc_context = $_SERVER['CONTEXT_DOCUMENT_ROOT'];
 		$prefix = $_SERVER['CONTEXT_PREFIX'];
+
+		$dir = str_replace("\\", "/", $dir);
 
 		if(strpos($dir, $doc_context) === 0){
 			$dir = substr($dir, strlen($doc_context));
 		}
 
-		return $prefix.$dir;
+		return $prefix.$dir.'/'.$relative;
 	}
 
-	define('BASEPATH', contextPath().'/');
+	define('BASEPATH', contextPath());
 ?>
--- test/test-path.php
+++ test/test-path.php
@@ -4,5 +4,6 @@
 	header("Content-Type: text/plain; charset=utf-8");
 
 	echo BASEPATH."xxx/yyy/zzzz".PHP_EOL;
+	echo __DIR__.PHP_EOL;
 	var_dump($_SERVER);
 ?>
